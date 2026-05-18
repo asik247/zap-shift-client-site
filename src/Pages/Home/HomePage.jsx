2267cf0b0d50e0ee5bc13398390cdea313b8cc8f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-19 01:39:35 +0600

    carasole explore

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const HomePage = () => {
     return (
         <div>
             <p>hi iam home pages</p>
+            <Banner></Banner>
         </div>
     );
 };
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div className='  '>
+        <div className=' text-red-400 '>
             <p>Services here</p>
            
         </div>
