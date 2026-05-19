88d3885f56772eaa8ed367022803143bd3c4f149
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-20 00:28:44 +0600

    coverage done

--- src/Pages/CoverPage/CoverPage.jsx
+++ src/Pages/CoverPage/CoverPage.jsx
@@ -14,16 +14,20 @@ const CoverPage = () => {
 
             </div>
             <div className='border w-full h-[800px]'>
-                <MapContainer center={position} zoom={10} scrollWheelZoom={false} className='h-[800px]'>
+                <MapContainer center={position} zoom={8} scrollWheelZoom={false} className='h-[800px]'>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
+                    {
+                        services.map((singleServic, index) =>
+                            <Marker key={index} position={[singleServic.latitude,singleServic.longitude]}>
+                                <Popup>
+                                   <strong>{singleServic.district}</strong> <br /> Services Area:{singleServic.covered_area.join(' , ')}
+                                </Popup>
+                            </Marker>
+                        )
+                    }
                 </MapContainer>
             </div>
         </div>
