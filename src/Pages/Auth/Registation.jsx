6de03fb0cfc1378787606dbc00f4c974e3d6ae88
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-20 12:25:02 +0600

    using react hook form and validation

--- src/Pages/Auth/Registation.jsx
+++ src/Pages/Auth/Registation.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const Registation = () => {
-    const {register,handleSubmit} = useForm()
-    const handleRegistater = (data)=>{
-        console.log('resigatio data;',data);
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const handleRegistater = (data) => {
+        console.log('resigatio data;', data);
 
     }
     return (
@@ -12,11 +12,13 @@ const Registation = () => {
             <form onSubmit={handleSubmit(handleRegistater)}>
                 <fieldset className="fieldset">
                     <label className="label">Email</label>
-                    <input type="email" {...register("email")} className="input" placeholder="Email" />
+                    <input type="email" {...register("email",{required:true})} className="input" placeholder="Email" />
+                    {errors.email?.type === 'required' && <p className='text-red-600'>Email is Require</p>}
                     <label className="label">Password</label>
                     {/* password field */}
-                    <input type="password" {...register("password")} className="input" placeholder="Password" />
-                    
+                    <input type="password" {...register("password",{required:true,minLength:6})} className="input" placeholder="Password" />
+                    {errors.password?.type ==='required' &&<p className='text-red-600'>Password is requre</p>}
+                    {errors.password?.type ==='minLength' &&<p className='text-red-600'>Password must be 6 character or longer</p>}
                     <button className="btn btn-neutral mt-4">Registation</button>
                 </fieldset>
             </form>
