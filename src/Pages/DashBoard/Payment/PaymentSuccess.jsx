9e0014ffbcea08a649af3839b1eafc0eb911eb21
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-26 00:13:24 +0600

    transtionId and trackingid implement  done

--- src/Pages/DashBoard/Payment/PaymentSuccess.jsx
+++ src/Pages/DashBoard/Payment/PaymentSuccess.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
 import useInstance from '../../../Hooks/useInstance';
 const PaymentSuccess = () => {
+    const [paymentInfo,setPaymentInfo] = useState({})
     const instance = useInstance();
     //!get success_id with useSearch Paremes;
     const [searchParams] = useSearchParams()
@@ -10,11 +11,18 @@ const PaymentSuccess = () => {
         instance.patch(`/payment-success?session_id=${sessionId}`)
         .then(res=>{
             console.log(res.data);
+            setPaymentInfo({
+                transactionId:res.data.transactionId,
+                trackingId:res.data.trackingId
+            })
         })
     },[instance,sessionId])
     return (
         <div>
             <h1>your pay successfully done</h1>
+            <br />
+            <p>Your TranstationId : {paymentInfo.transactionId}</p>
+            <p>Your TrackingId : {paymentInfo.trackingId}</p>
         </div>
     );
 };
