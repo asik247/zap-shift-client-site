311762e7ede9f1be4ceeedeb43e71d7da120ba39
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-27 18:38:16 +0600

    db get parcel data

--- src/Pages/DashBoard/MyParcel2/MyParcel2.jsx
+++ src/Pages/DashBoard/MyParcel2/MyParcel2.jsx
@@ -1,9 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-
+import useAuth from '../../../Hooks/useAuth';
+import useInstance from '../../../Hooks/useInstance';
 const MyParcel2 = () => {
+    //?TranStack Query using load parcles data in db;
+    const {user} = useAuth();
+    const instance = useInstance()
+    const {data:parcel = [],isLoading} = useQuery({
+        queryKey:['parcels',user?.email],
+        queryFn:async()=>{
+            const res = await instance(`/percelDatas?${user?.email}`);
+            return res.data
+        }
+    })
+    //?Loading state;
+    if(isLoading){
+        return <p>Loading...</p>
+    }
     return (
         <div>
-            <p>My Parcel 2 here!</p>
+            <p>My Parcel 2 here!{parcel.length}</p>
             {/* 
             1.db get sender parcels data;
             2.show table formate;
