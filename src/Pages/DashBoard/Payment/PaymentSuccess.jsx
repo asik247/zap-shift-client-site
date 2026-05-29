9a790131fb2ff53912ea24ab6fa32ffaebee1846
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-29 17:04:40 +0600

    retrive session revision done

--- src/Pages/DashBoard/Payment/PaymentSuccess.jsx
+++ src/Pages/DashBoard/Payment/PaymentSuccess.jsx
@@ -1,20 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
 import useInstance from '../../../Hooks/useInstance';
 const PaymentSuccess = () => {
+    const [paymentInfo,setPaymentInfo] = useState([]);
     const instance = useInstance();
     const [searchParams] = useSearchParams();
     const sessionId = searchParams.get('session_id');
     //!UseEffect send session id in server;
     useEffect(()=>{
-        instance.patch(`/payment-success?session_id=${sessionId}`)
+        instance.patch(`/payment-success?session_id=${sessionId}`)  
         .then(res=>{
             console.log(res.data);
+            setPaymentInfo(res.data)
         })
     },[instance,sessionId])
     return (
         <div>
             <p>Pay successfully done✅</p>
+            <p>TrackingId:${paymentInfo.trackingId} and TransitionId:${paymentInfo.transactionId}</p>
         </div>
     );
 };
