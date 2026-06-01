c24dd40c6a726524954a7178f322907288c8812f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-06-01 10:41:09 +0600

    reject

--- src/Pages/DashBoard/ApproveRider/ApproveRider.jsx
+++ src/Pages/DashBoard/ApproveRider/ApproveRider.jsx
@@ -1,6 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useInstance from '../../../Hooks/useInstance';
+import { FcApprove, FcDisapprove } from 'react-icons/fc';
+import Swal from 'sweetalert2';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const ApproveRider = () => {
     const instance = useInstance()
@@ -12,15 +15,30 @@ const ApproveRider = () => {
         }
     })
     //? approvel;
-    const handlerApproval = (id)=>{
-        // console.log('id',id);
-        const updateInfo = {status:'approved'}
-        instance.patch(`/riders/${id}`,updateInfo)
-        .then(res=>{
-            console.log(res.data);
-           
-        })
+    const handlerApproval = (id) => {
+        const updateInfo = { status: 'Approved' }
+        instance.patch(`/riders/${id}`, updateInfo)
+            .then(res => {
+                if (res.data.modifiedCount) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "your application has been approved!",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
 
+            })
+
+    }
+    //? Rejected;
+    const handlerRejected = (id) => {
+        const updateInfo = { status: 'Rejected' }
+        instance.patch(`/riders/${id}`, updateInfo)
+            .then(res => {
+                console.log(res.data);
+            })
     }
     return (
         <div>
@@ -30,30 +48,39 @@ const ApproveRider = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>SL.NO</th>
                             <th>Name</th>
                             <th>Status</th>
+                            <th>Email</th>
+                            <th>Region</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             riders.map((rider, index) =>
-                            
-                            <tr key={rider._id}>
-                                <th>{index+1}</th>
-                                <td>Cy Ganderton</td>
-                                <td>
-                                    <p >{rider.status}</p>
-                                    
-                                </td>
-                                <td>
-                                    <button onClick={()=>handlerApproval(rider._id)} className="btn mx-2">accept</button>
-                                    <button className="btn">reject</button>
-                                </td>
-                            </tr>)
+
+                                <tr key={rider._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{rider.name}</td>
+                                    <td>
+                                        <p >{rider.status}</p>
+                                    </td>
+                                    <td>
+                                        <p >{rider.email}</p>
+                                    </td>
+                                    <td>
+                                        <p >{rider.region}</p>
+                                    </td>
+                                    <td>
+                                        <button onClick={() => handlerApproval(rider._id)} className="btn mx-2"><FcApprove />
+                                        </button>
+                                        <button onClick={() => handlerRejected(rider._id)} className="btn mx-2"><FcDisapprove /></button>
+                                        <button className="btn"><FaTrashAlt /></button>
+                                    </td>
+                                </tr>)
                         }
-                       
+
 
 
                     </tbody>
