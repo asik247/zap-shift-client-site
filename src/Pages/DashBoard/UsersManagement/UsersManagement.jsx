689b54dad17080ab225d5f0a6abdc0a112ef8236
Asikur Rahaman <mdasikur5893@gmail.com>
2026-06-05 23:08:30 +0600

    users data load and show table formate

--- src/Pages/DashBoard/UsersManagement/UsersManagement.jsx
+++ src/Pages/DashBoard/UsersManagement/UsersManagement.jsx
@@ -5,16 +5,65 @@ import useInstance from '../../../Hooks/useInstance';
 const UsersManagement = () => {
     const instance = useInstance()
     //Todo Transtack querey using load users data in db;
-    const {data:users = []} = useQuery({
-        queryKey:['users'],
-        queryFn:async()=>{
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
             const res = await instance(`/users`);
             return res.data
         }
     })
     return (
         <div>
-             <h2 className="text-4xl">Manage users {users.length}</h2>
+            <h2 className="text-4xl">Manage users {users.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                               #
+                            </th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => <tr>
+                            <td>
+                               {index+1}
+                            </td>
+                            <td>
+                                {/* Photo + name */}
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={user?.photoURL}
+                                                alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{user?.displayName}</div>
+                                        
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                               {user.email}
+                            </td>
+                            <td>Purple</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                            </th>
+                        </tr>)}
+
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
