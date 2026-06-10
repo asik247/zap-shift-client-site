0fd0c162172340eb231bafb0e62d676c6ba81e43
Asikur Rahaman <mdasikur5893@gmail.com>
2026-06-10 12:25:41 +0600

    rider accept then status update rider-arriving

--- src/Pages/DashBoard/AssignDeliverys/AssignDeliverys.jsx
+++ src/Pages/DashBoard/AssignDeliverys/AssignDeliverys.jsx
@@ -2,17 +2,35 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAuth from '../../../Hooks/useAuth';
 import useInstance from '../../../Hooks/useInstance';
+import Swal from 'sweetalert2';
 
 const AssignDeliverys = () => {
     const { user } = useAuth();
     const instance = useInstance();
-    const { data: parcels = [] } = useQuery({
+    const { refetch, data: parcels = []  } = useQuery({
         queryKey: ['parcels', user?.email, 'driver-assign'],
         queryFn: async () => {
             const res = await instance.get(`/percelDatas/rider?riderEmail=${user?.email}&deliveryStatus=driver-assign`)
             return res.data
         }
     })
+    //? handler accept delivery;
+    const handlerAcceptDelivery = (parcle) => {
+        const statusInfo = { deliveryStatus: 'rider-arriving' };
+        instance.patch(`/percelDatas/${parcle._id}/status`, statusInfo)
+            .then(res => {
+                if (res.data.modifiedCount) {
+                    refetch()
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Thank you accept this parcle",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    }
     return (
         <div>
             <p>Parcels Pending Pickup {parcels.length}</p>
@@ -24,7 +42,7 @@ const AssignDeliverys = () => {
                         <tr>
                             <th></th>
                             <th>Name</th>
-                            <th>Job</th>
+                            <th>Confarm</th>
                             <th>Favorite Color</th>
                         </tr>
                     </thead>
@@ -33,11 +51,14 @@ const AssignDeliverys = () => {
                         {/* parcels maping and showing table formate */}
                         {
                             parcels.map((parcle, i) => <tr key={i}>
-                            <th>{i + 1}</th>
-                            <td>{parcle.percelName}</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>)
+                                <th>{i + 1}</th>
+                                <td>{parcle.percelName}</td>
+                                <td>
+                                    <button onClick={() => handlerAcceptDelivery(parcle)} className="btn bg-green-600-400">Accept</button>
+                                    <button className="btn ms-2 bg-red-500">Reject</button>
+                                </td>
+                                <td>Blue</td>
+                            </tr>)
                         }
 
                     </tbody>
