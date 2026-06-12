0649240fe9f1ec69090fa491cc7f5a61811d2c2e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-06-12 22:47:11 +0600

    showing rechart

--- src/Pages/DashBoard/DashBoardHome/DashBoardAdminHome.jsx
+++ src/Pages/DashBoard/DashBoardHome/DashBoardAdminHome.jsx
@@ -1,15 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useInstance from '../../../Hooks/useInstance';
+import { Legend, Pie, PieChart, Tooltip } from 'recharts';
+
 const DashBoardAdminHome = () => {
     const instance = useInstance();
     const { data: delivery = [] } = useQuery({
-        queryKey: ['delivery', 'deliveryStatus'],
+        queryKey: ['delivery-status-stats'],
         queryFn: async () => {
             const res = await instance.get('/percelDatas/delivery-status/stats');
             return res.data;
         }
     })
+    //? showing pie data;
+    const getPyChartData = (data) => {
+        return data.map(item => {
+            return { name: item.status, value: item.count }
+        })
+    }
     return (
         <div>
             <h2 className="text-4xl">DashBoard Admin Home Page!</h2>
@@ -26,7 +34,29 @@ const DashBoardAdminHome = () => {
 
             </div>
 
+            <div className='w-full h-[400px]'>
+                <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 2 }} responsive>
+                    <Pie
+                        dataKey="value"
+                        startAngle={180}
+                        endAngle={0}
+                        data={getPyChartData(delivery)}
+                        cx="50%"
+                        cy="100%"
+                        outerRadius="120%"
+                        fill="#8884d8"
+                        label
+                        isAnimationActive={true}
+
+                    />
+                    <Legend></Legend>
+                    <Tooltip></Tooltip>
+
+                </PieChart>
+            </div>
+
         </div>
+
     );
 };
 
